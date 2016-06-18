30a483137e9a2a7373884ab5dded2df3d94305e9
charliechauri <carlos.echauri@gmail.com>
2016-06-18 13:36:56 -0500

    feat Añade estilos para navegación superior
    Añade script principal

---
+++ index.js
@@ -0,0 +1,18 @@
+/*(function(){
+    'use strict';
+
+    var nombres = ['Edgar', 'Carlos', 'Alberto', 'Oswaldo'];
+
+    window.onload = function () {
+        var ul = document.getElementById('list');
+
+        nombres.forEach(function(nombre){
+            var li = document.createElement('li');
+            li.innerText = nombre;
+            ul.appendChild(li);
+        });
+
+        
+    };
+})();*/
+
